091caae2265536870ab2be13a028f3985bb9c69d
sangramsam <singh.sangram56@gmail.com>
2017-07-28 23:15:21 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 var app = angular.module("app", []);
-app.controller("main", function ($scope) {
-    $scope.a = 12;
-    $scope.b = 10;
+app.controller("ctrl1", function ($scope) {
+    $scope.emp = [
+        {
+            "name": "sangram",
+            "profile": "Front End Developer"
+        }, {
+            "name": "akash",
+            "profile": "Front End Developer"
+        }, {
+            "name": "Pratiksha",
+            "profile": "UI Developer"
+        }, {
+            "name": "Madhuri",
+            "profile": "UI Developer"
+        }
+    ];
+    console.log($scope.emp);
 
-    $scope.add = function () {
-        $scope.b += 10;
-        $scope.a += 10;
-        $scope.sum = $scope.a + $scope.b;
+    $scope.add = function (name, profile) {
+        var temp = {};
+        temp.name = name;
+        temp.profile = profile;
+        console.log(temp);
+        $scope.emp.push(temp);
+
     }
-    $scope.data = "Testing";
+    $scope.show = function (da) {
+        alert(da);
+    }
+    $scope.showText = true;
+    $scope.show = function () {
+        if ($scope.showText === false) {
+            $scope.showText = true;
+        } else {
+            $scope.showText = false;
+        }
 
+    }
 
-    $scope.login=function (name,email) {
-        $scope.username=name;
-        $scope.emails=email;
+    $scope.trackChange=function (data) {
+        $scope.track=data;
+
+    }
+    $scope.trackColor=function () {
+        if ($scope.over === false) {
+            $scope.over = true;
+        } else {
+            $scope.over = false;
+        }
+
     }
 });
+
+
+
