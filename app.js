2d22e51f1e52f068401547de1b0653800b5ebde9
sangramsam <singh.sangram56@gmail.com>
2017-08-01 09:34:44 +0530

    filter and custom filter

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ var app = angular.module("app", []);
 app.controller("ctrl1", function ($scope) {
     $scope.emp = [
         {
-            "name": "sangram",
-            "profile": "Front End Developer"
+            "name": "sangram singh",
+            "profile": "Front End Developer",
+            "date": new Date()
         }, {
-            "name": "akash",
-            "profile": "Front End Developer"
+            "name": "akash deep",
+            "profile": "Front End Developer",
+            "date": new Date()
+
         }, {
-            "name": "Pratiksha",
-            "profile": "UI Developer"
+            "name": "Pratiksha parsewaa",
+            "profile": "UI Developer",
+            "date": new Date()
         }, {
-            "name": "Madhuri",
-            "profile": "UI Developer"
+            "name": "Madhuri zyxx",
+            "profile": "UI Developer",
+            "date": new Date()
         }
     ];
     console.log($scope.emp);
@@ -38,16 +43,23 @@ app.controller("ctrl1", function ($scope) {
 
     }
 
-    $scope.trackChange=function (data) {
-        $scope.track=data;
+    $scope.trackChange = function (data) {
+        $scope.track = data;
 
     }
-    $scope.trackColor=function () {
+    $scope.trackColor = function () {
         if ($scope.over === false) {
             $scope.over = true;
         } else {
             $scope.over = false;
         }
+
+    }
+});
+app.filter("myfil", function () {
+    return function (value) {
+        console.log("inside filter",value);
+        return value.split(' ')[1];
 
     }
 });
