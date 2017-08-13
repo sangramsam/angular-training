1e3dfc2a8a4febe7c83501a4e92394c24a23b7ad
sangramsam <singh.sangram56@gmail.com>
2017-08-13 22:13:31 +0530

    services

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", []);
-app.controller("ctrl1", function ($scope) {
-    $scope.emp = [
+app.controller("ctrl1", function ($scope, git) {
+    /*$scope.emp = [
         {
             "name": "sangram singh",
             "profile": "Front End Developer",
@@ -55,12 +55,115 @@ app.controller("ctrl1", function ($scope) {
         }
 
     }
+
+    $scope.submit = function (username, password) {
+        console.log("data", username, password);
+    }
+
+    $scope.getuser = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:3000/getUser"
+        }).then(function (response) {
+            console.log(response);
+            $scope.data = response.data.user;
+        })
+    }
+    $scope.data = {name: 'sam', address: '123 Somewhere'};
+
+    $scope.trackValue = [{
+        "text": "ds"
+    }]
+    ;
+    var i = 1;
+    $scope.add = function (value) {
+        var tepp = {
+            "text": value
+        }
+        $scope.trackValue.push(tepp);
+        console.log(value);
+    }
+    $scope.blk = [
+        {
+            name: '',
+            email: '',
+            btn: 'add'
+        }
+    ]
+    $scope.addMore = function () {
+        $scope.blk.push({name: '', email: ''});
+    }
+
+    $scope.trackAddress = false;
+    $scope.bAddress = [];
+    $scope.sAddress = [
+        {
+            name: '',
+            No: '',
+            addr: ''
+        }
+    ];
+    $scope.save = function (bill) {
+        $scope.bAddress.push(bill);
+        console.log($scope.bAddress);
+    };
+    $scope.saveShop = function () {
+        console.log($scope.sAddress);
+    }
+
+    $scope.$watch('trackAddress', function () {
+        console.log("called");
+        if ($scope.trackAddress === true) {
+            $scope.sAddress = $scope.bAddress[0];
+            console.log($scope.sAddress);
+        } else {
+            $scope.sAddress = [
+                {
+                    name: '',
+                    No: '',
+                    addr: ''
+                }
+            ];
+        }
+    });*/
+    var name="pratikshaparsewar"
+
+    git.getUser(name).then(function (response) {
+        console.log(response)
+    });
 });
 app.filter("myfil", function () {
     return function (value) {
-        console.log("inside filter",value);
+        console.log("inside filter", value);
         return value.split(' ')[1];
 
+    }
+});
+app.directive("customElement", function () {
+    return {
+        restrict: 'E',
+        scope: {
+            data: '=id'
+        },
+        templateUrl: 'custom.html'
+    }
+})
+app.factory("git", function ($http, $q) {
+    function getUser(name) {
+        var deferred = $q.defer();
+        $http({
+            url: "https://api.github.com/users/"+name,
+            method: "get"
+        }).then(function (response) {
+            deferred.resolve({
+                data: response.data
+            });
+        });
+        return deferred.promise;
+    }
+
+    return {
+        getUser: getUser
     }
 });
 
